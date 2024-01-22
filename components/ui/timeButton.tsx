ee4f24f59864c452038409a7905f109dc14a60d7
SUNGWON111 <user@userui-MacBookPro.local>
2024-01-22 21:37:50 +0900

    create reservation_page ui

---
+++ components/ui/timeButton.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button';
+
+const Time: React.FC = () => {
+  return (
+    <>
+      <div>오전</div>
+      <Button variant='outline'>09:00</Button>
+      <Button variant='outline'>10:00</Button>
+      <Button variant='outline'>11:00</Button>
+      <div>오후</div>
+      <Button variant='outline'>12:00</Button>
+      <Button variant='outline'>01:00</Button>
+      <Button variant='outline'>02:00</Button>
+      <Button variant='outline'>03:00</Button>
+      <Button variant='outline'>04:00</Button>
+      <Button variant='outline'>05:00</Button>
+      <Button variant='outline'>06:00</Button>
+      <Button variant='outline'>07:00</Button>
+      <Button variant='outline'>08:00</Button>
+    </>
+  );
+};
+export default Time;
