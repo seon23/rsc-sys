c1ce7d3f059095062d66097d16d32c704555e4ed
seon23 <pqpqpq460@gmail.com>
2024-01-24 20:07:43 +0900

    Install nextAuth and configure for Google social login

---
+++ app/_lib/auth.ts
@@ -0,0 +1,18 @@
+// import Auth from '@auth/core';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET as string,
+});
